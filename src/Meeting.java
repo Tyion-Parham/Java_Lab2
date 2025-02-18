30d0270372b3f550a29a554f750b24ecd8f34e79
Tyion-Parham <112714623+Tyion-Parham@users.noreply.github.com>
2025-02-18 17:08:29 -0600

    Adds Meeting, validates with EventTester

---
+++ src/Meeting.java
@@ -0,0 +1,48 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Meeting extends Event implements Completable{
+    private LocalDateTime endDateTime;
+    private String location;
+    Boolean complete = false;
+
+    public Meeting(String meetingOrintation, LocalDateTime start, LocalDateTime end, String location) {
+        setName(meetingOrintation);
+        endDateTime = end;
+        this.location = location;
+        setDateTime(start);
+    }
+
+    @Override
+    public void complete() {
+        complete = true;
+    }
+
+    @Override
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public LocalDateTime getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(LocalDateTime endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Duration getDuration() {
+        Duration duration;
+        duration = Duration.ofHours(ChronoUnit.HOURS.between(getDateTime(), endDateTime));
+        return duration;
+    }
+}
